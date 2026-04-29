169618d81877ad31fc88e149633a7359f493bea5
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-04-29 16:29:40 -0600

    .env.example/config_loader: add requests config for Crossref and DOI + test directories

--- config_loader.py
+++ config_loader.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# work and output directories
 WORK_DIR_PATH = os.getenv("WORK_DIR_PATH", "output")
-# Main output directory
 OUTPUT_DIR_PATH = os.getenv("OUTPUT_DIR_PATH", "output")
-# Dedicated test directory
+# Dedicated test directories
+TEST_WORK_DIR_PATH = os.getenv("TEST_WORK_DIR_PATH", "tests_output")
 TEST_OUTPUT_DIR_PATH = os.getenv("TEST_OUTPUT_DIR_PATH", "tests_output")
 
 # Crossref Rest API
@@ -19,13 +20,18 @@ CROSSREF_API_STYLES_URL = os.getenv("CROSSREF_API_STYLES_URL", "")
 CROSSREF_API_WORKS_URL = os.getenv("CROSSREF_API_WORKS_URL", "")
 CROSSREF_API_MAX_RESULTS = int(os.getenv("CROSSREF_API_MAX_RESULTS", 6))
 CROSSREF_API_TIMEOUT = float(os.getenv("CROSSREF_API_TIMEOUT", 10))
+CROSSREF_API_MAX_RETRY = int(os.getenv("CROSSREF_API_MAX_RETRY", 10))
 
 # DOI Content service negotiation
 DOI_API_DELAY = float(os.getenv("DOI_API_DELAY", 0.4))
 DOI_API_URL = os.getenv("DOI_API_URL", "")
 DOI_API_TIMEOUT = float(os.getenv("DOI_API_TIMEOUT", 10))
+DOI_API_MAX_RETRY = int(os.getenv("DOI_API_MAX_RETRY", 10))
 
 CONTEXT_KEYWORDS = os.getenv("CONTEXT_KEYWORDS", "")
+
+JOURNAL_UPDATE_DAYS = os.getenv("JOURNAL_UPDATE_DAYS", 30)
+JOURNAL_UPDATE_MAX = os.getenv("JOURNAL_UPDATE_MAX", 100)
 
 PARSER_DEFAULT_BLACKLIST = ['Fig', 'Figs', 'Figure', 'Figures', 'Tab', 
                             'Table', 'Eq', 'Plate', 'Section', 'See', 'e.g.', 'i.e.', 
@@ -34,6 +40,6 @@ PARSER_DEFAULT_BLACKLIST = ['Fig', 'Figs', 'Figure', 'Figures', 'Tab',
                             'December']
 
 # Ensure directories exist
-for d in [WORK_DIR_PATH, OUTPUT_DIR_PATH, TEST_OUTPUT_DIR_PATH]:
+for d in [WORK_DIR_PATH, OUTPUT_DIR_PATH, TEST_WORK_DIR_PATH, TEST_OUTPUT_DIR_PATH]:
     if not Path(d).is_dir():
         os.makedirs(d)
