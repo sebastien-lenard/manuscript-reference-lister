49c8014e68a3be73b5c4f6482220cd012464c90c
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-05-02 17:55:42 -0600

    src: renamed scripts to follow repository / metadata logic

--- src/manuscript_reference_lister/__init__.py
+++ src/manuscript_reference_lister/__init__.py
@@ -1,13 +1,13 @@
 from . import config_loader
 from .citation_parser import CitationParser
 from .data_loader import DataLoader
-from .doi_fetcher import DoiFetcher
-from .journal_fetcher import JournalFetcher
+from .doi_repository import DoiFetcher
 from .journal_parser import JournalParser
+from .journal_repository import JournalFetcher
 from .requests_wrapper import RequestsWrapper
-from .style_fetcher import StyleFetcher
-from .work import Work
-from .work_fetcher import WorkFetcher
+from .style_repository import StyleFetcher
+from .work_metadata import Work
+from .work_repository import WorkFetcher
 
 # Warning: don't include packages that can call themselves in a circular way
 __all__ = [
