1420bbd584c15800a406a9cadbbd13a114972516
Sebastien Lenard <sebastien.lenard@gmail.com>
2026-05-03 11:00:50 -0600

    [minor] schemas work/journal: add examples and removed score field.

--- src/manuscript_reference_lister/schemas/journal_metadata.py
+++ src/manuscript_reference_lister/schemas/journal_metadata.py
@@ -2,10 +2,10 @@ from typing import TypedDict
 
 
 class JournalMetadata(TypedDict):
-    input_title: str
-    true_title: str
-    publisher: str
-    ISSN: str
-    start_year: int
-    end_year: int
-    update: str
+    input_title: str  # e.g. Nature Geoscience
+    true_title: str  # e.g. Nature Geoscience
+    publisher: str  # e.g. Nature Portfolio / Springer Nature
+    ISSN: str  # e.g. 1752-0894
+    start_year: int  # e.g. 2008
+    end_year: int  # e.g. 2026
+    update: str  # e.g. 2026-03-27
--- src/manuscript_reference_lister/schemas/work_metadata.py
+++ src/manuscript_reference_lister/schemas/work_metadata.py
@@ -2,11 +2,10 @@ from typing import TypedDict
 
 
 class WorkMetadata(TypedDict):
-    input_first_authors: str
-    input_year_and_suffix: int
-    input_ISSN: str
-    reference: str
-    style: str
-    doi: str
-    score: int
-    type: str
+    input_first_authors: str  # e.g. Lenard et al., Guns and Vanacker
+    input_year_and_suffix: int  # e.g. 2020a
+    input_ISSN: str  # e.g. 1752-0894
+    reference: str  # e.g. Lenard, S. J. P., Lavé, J., France-Lanord, C., Aumaître, G., Bourlès, D. L., & Keddadouche, K. (2020). Steady erosion rates in the Himalayas through late Cenozoic climatic changes. Nature Geoscience, 13(6), 448–452. https://doi.org/10.1038/s41561-020-0585-2
+    style: str  # e.g. apa
+    doi: str  # e.g. 10.1038/s41561-020-0585-2
+    type: str  # e.g. journal-article
